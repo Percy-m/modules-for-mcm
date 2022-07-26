8b9a04b0ecbe896f1b6bd99a2ee5d013beeb2e0b
Jiayi Zhu <2984921744@qq.com>
2022-07-26 19:10:25 +0800

    fit_plot

--- fi/fitting.py
+++ fi/fitting.py
@@ -2,36 +2,77 @@
 author: Jiayi Zhu
 """
 
-from typing import Iterable, Union
+from typing import Iterable, Union, Callable
 from matplotlib import pyplot
+from scipy import optimize
 import numpy as np
 
 
 def poly_fit_plot(x: Union[np.ndarray, Iterable, int, float],
                   y: Union[np.ndarray, Iterable, int, float],
-                  degree: int,
+                  mode: int,
+                  deg: int = None,
+                  func: Callable = None,
+                  draw=True,
+                  data_label: str = None,
+                  fit_label: str = None,
                   pic_name='a.png'):
     """
     多项式拟合， 并画出原数据散点图与拟合曲线
-    :param x:
-    :param y:
-    :param degree:
-    :param pic_name:
-    :return:
+
+    示例
+    ----
+    >>> x_ = np.array([1, 2, 3, 4, 5])
+    >>> y_ = np.array([2, 4, 4, 5, 6])
+    >>> c, r_squared = poly_fit_plot(x_, y_, deg=1,
+    ... data_label='原数据',
+    ... fit_label='拟合曲线',
+    ... pic_name='一次拟合.png')
+
+    :param x: 自变量
+    :param y: 因变量
+    :param mode: 值为 0 时，为多项式拟合；值为 1 时，为曲线拟合
+    :param deg: 多项式次数，mode=0时必填
+    :param func: 拟合的函数，mode=1时必填
+    :param draw: 是否绘图
+    :param data_label: 原数据标签
+    :param fit_label: 拟合曲线标签
+    :param pic_name: 保存的图片名
+    :return: 多项式从高到低次幂系数，
+             R方
     """
-    p = np.polyfit(x, y, degree)
-    pyplot.scatter(x, y)
-    func = 0
-    for i in range(degree + 1):
-        it = np.power(x, degree - i)
-        func += it * p[i]
-    pyplot.plot(x, func)
-    pyplot.savefig(pic_name, dpi=500)
-    pyplot.show()
-    return p
+    if mode == 0:
+        if deg is None:
+            raise Exception("deg must be int")
+        p = np.polyfit(x, y, deg)
+        f = 0
+        for i in range(deg + 1):
+            it = np.power(x, deg - i)
+            f += it * p[i]
+    elif mode == 1:
+        if func is None:
+            raise Exception("func must be Callable")
+        p, _ = optimize.curve_fit(func, x, y)
+        f = func(x, p)
+    else:
+        raise Exception("mode must be 0 or 1")
+    r_s = np.corrcoef(y, f)[0, 1]
+    if draw:
+        pyplot.rc('font', family='SimHei')
+        pyplot.scatter(x, y, label=data_label)
+        pyplot.plot(x, func, label=fit_label)
+        pyplot.legend()
+        pyplot.savefig(pic_name, dpi=500)
+        pyplot.show()
+    return p, r_s ** 2
 
 
 if __name__ == '__main__':
-    x = np.array([1, 2, 3, 4, 5])
-    y = np.array([2, 4, 4, 5, 6])
-    print(poly_fit_plot(x, y, 1))
+    x_1 = np.array([0, 1, 2, 3, 4])
+    y_1 = np.array([2, 4, 5, 8, 10])
+    print(poly_fit_plot(x_1, y_1,
+                        deg=1,
+                        data_label='原数据',
+                        fit_label='拟合曲线',
+                        pic_name=r"C:\Users\ANO4679\Desktop\a.png"))
+    pass
