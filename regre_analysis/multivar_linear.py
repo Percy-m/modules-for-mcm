bbc635d494c7c5c53df8fa327913aa48448aabc8
Jiayi Zhu <2984921744@qq.com>
2022-08-02 21:48:32 +0800

    fit check

--- regre_analysis/multivar_linear.py
+++ regre_analysis/multivar_linear.py
@@ -8,8 +8,7 @@ from sklearn.linear_model import LinearRegression
 class MultiVariableLinear:
 
     def __init__(self, data_x, data_y):
-        self.data_x = data_x
-        self.data_y = data_y
-        model = LinearRegression().fit(self.data_x, self.data_y)
+        self._model = LinearRegression()
+        self._model.fit(data_x, data_y)
 
     pass
