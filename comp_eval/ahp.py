88aafe49bff1bae29f1f40c6b9efcbf4efa47c8d
Jiayi Zhu <2984921744@qq.com>
2022-08-03 21:02:51 +0800

    weight_vector

--- comp_eval/ahp.py
+++ comp_eval/ahp.py
@@ -2,6 +2,7 @@
 author: Jiayi Zhu
 """
 from scipy.sparse.linalg import eigs
+from scipy.linalg import eig, eigvals
 import numpy as np
 
 RI = [
@@ -23,33 +24,74 @@ RI = [
 ]
 
 
-def mat_cr(mat_a):
+def mat_cr(mat_a: np.ndarray):
     """
     计算一致性比率
 
     输入判断矩阵的阶数应在 3~15 之间
-    :param mat_a:
+    :param mat_a: 输入矩阵
     :return:
     """
     l_a, _ = eigs(mat_a, 1)
     len_ = len(mat_a)
     if 15 >= len_ > 2:
-        cr_a = (l_a - len_)/len_ - 1/RI[len_ - 1]
+        cr_a = (l_a - len_)/(len_ - 1)/RI[len_ - 1]
     else:
         raise NotImplemented
     return cr_a
 
 
-def max_lambda(mat_a):
-    _, v = eigs(mat_a, 1)
-    return v
+def max_lambda(mat_a: np.ndarray):
+    """
+    计算最大特征值
+
+    """
+    lambda_ = np.real(eig(mat_a)[0])
+    return np.max(lambda_)
 
 
-#
+def weight_vec(mat_a: np.ndarray):
+    """
+    .. math:: AX = \\lambda_{max} X
+
+    将 X 归一化作为权向量 W
+
+    :param mat_a: 输入矩阵
+    :return: 返回 W
+    """
+    lambda_ = np.real(eig(mat_a)[0])
+    vec_ = eig(mat_a)[1].T
+    v = []
+    for i in range(len(lambda_)):
+        if lambda_[i] == np.max(lambda_):
+            v = vec_[i]
+    return v/sum(v)
+
 
 if __name__ == '__main__':
-    a = [[1, 3, 4],
-         [1/3, 1, 2],
-         [1/4, 1/2, 1]]
-    print(eigs(np.array(a), 1))
+    a = [
+        [  1,   7,   5, 1/3,   5,   3,   1,   7,   5,   5,   1,   6],
+        [1/7,   1, 1/3, 1/5, 1/2, 1/4, 1/6,   1, 1/2, 1/2, 1/6,   1],
+        [1/5,   3,   1, 1/5,   1, 1/2, 1/4,   2,   1,   1, 1/4,   2],
+        [  3,   5,   5,   1,   5,   3,   1,   7,   5,   5,   1,   6],
+        [1/5,   2,   1, 1/5,   1, 1/2, 1/4, 1/2,   1,   1, 1/4, 1/2],
+        [1/3,   4,   4, 1/3,   2,   1, 1/2,   4,   2,   2, 1/2,   3],
+        [  1,   6,   4,   1,   4,   2,   1,   6,   4,   4,   1,   5],
+        [1/7,   1, 1/2, 1/7,   2, 1/4, 1/6,   1, 1/2, 1/2, 1/6, 1/2],
+        [1/5,   2,   1, 1/5,   1, 1/2, 1/4,   2,   1,   1, 1/4,   2],
+        [1/5,   2,   1, 1/5,   1, 1/2, 1/4,   2,   1,   1, 1/4,   2],
+        [  1,   6,   4,   1,   4,   2,   1,   6,   4,   4,   1,   5],
+        [1/6,   1, 1/2, 1/6,   2, 1/3, 1/5,   2, 1/2, 1/5, 1/5,   1]
+    ]
+    d = np.array(a)
+    l_a_, _ = eigs(d, 1)
+    # print(l_a_)
+    # print(len(d))
+    # print(mat_cr(d))
+    m = np.array([[1, 3], [1/3, 1]])
+    l_ = eig(m)
+    print(l_[0])
+    print(weight_vec(m))
+
+
 
