e95f3e922b04719f55e376a4ba3c7ebf642b05e7
Jiayi Zhu <2984921744@qq.com>
2022-08-08 20:04:52 +0800

    hierarchy cluster

--- multi_analysis/cluster.py
+++ multi_analysis/cluster.py
@@ -9,17 +9,47 @@ author: Jiayi Zhu
 （5）画聚类谱系图
 （6）决定类的个数及包含各类的样品数，并对类做出解释
 """
+
+
 import numpy as np
+from numpy import ndarray
 from sklearn import preprocessing as _pp
 import scipy.cluster.hierarchy as _sch
+from scipy.spatial.distance import pdist, squareform
+from scipy.cluster.hierarchy import linkage
 import matplotlib.pyplot as _plt
 
 
-# def hiera_cluster(
-#         data: np.ndarray
-# ):
-#
-#     pass
+def hiera_cluster(data: ndarray,
+                  method='single',
+                  labels: ndarray = None,
+                  pic_path='a.png'):
+    """
+    层次聚类，两两对象间的距离采用欧式距离，同时绘制聚类图
+
+    method 可选参数有：
+
+    'single' 最短距离法（默认）
+    'average' 无权平均距离
+    'centroid' 重心距离
+    'complete' 最大距离
+    'ward' 离差平方和方法
+
+    :param data: 输入的数据
+    :param method: 使用的方法
+    :param labels: 聚类图横轴的标签，默认为0, 1, 2, ...
+    :param pic_path: 聚类图保存路径
+    :return: 矩阵格式的两两对象间距离 和 linkage后的聚类结果
+    """
+    format_data = _pp.minmax_scale(data)
+    dist = pdist(format_data)
+    mat_dist = squareform(dist)
+    lk = linkage(d, method=method)
+    _plt.rc('font', family='SimHei')
+    _sch.dendrogram(lk, labels=labels)
+    _plt.savefig(pic_path)
+    _plt.show()
+    return mat_dist, lk
 
 
 if __name__ == '__main__':
@@ -38,10 +68,15 @@ if __name__ == '__main__':
     z = _sch.linkage(d)
 
     print(f_data)
+    print()
     print(d)
+    print()
     print(mat_d)
+    print()
     print(z)
-    s = np.array([str(i+1) for i in range(7)])
-    _plt.rc('font', size=16)
+    s = np.array([1, 2, 3, 4, 5, 6, 7])
+    _plt.rc('font')
     _sch.dendrogram(z, labels=s)
     _plt.show()
+
+    hiera_cluster(data_, 'complete', labels=s)
